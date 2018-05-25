70162f3f74565a5317148ce659176968c3d3679c
C Ghercoias <8409018+cghercoias@users.noreply.github.com>
2018-05-25 14:28:04 -0400

    Update strandtest.ino

--- examples/strandtest/strandtest.ino
+++ examples/strandtest/strandtest.ino
@@ -1,28 +1,28 @@
 #include "LPD8806.h"
 #include "SPI.h" // Comment out this line if using Trinket or Gemma
 #ifdef __AVR_ATtiny85__
- #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 // Example to control LPD8806-based RGB LED Modules in a strip
 
 /*****************************************************************************/
 
-// Number of RGB LEDs in strand:
-int nLEDs = 32;
+// Number of RGB LEDs in strand (30/m, 48/m, 60/m):
+int nLEDs = 60;
 
 // Chose 2 pins for output; can be any valid output pins:
-int dataPin  = 2;
-int clockPin = 3;
+int dataPin  = 11;
+int clockPin = 13;
 
 // First parameter is the number of LEDs in the strand.  The LED strips
-// are 32 LEDs per meter but you can extend or cut the strip.  Next two
+// are 32/48/60 LEDs per meter but you can extend or cut the strip.  Next two
 // parameters are SPI data and clock pins:
 LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
 
 // You can optionally use hardware SPI for faster writes, just leave out
 // the data and clock pin parameters.  But this does limit use to very
-// specific pins on the Arduino.  For "classic" Arduinos (Uno, Duemilanove,
+// specific pins on the Arduino.  For "classic" Arduinos (Uno, Nano, Duemilanove,
 // etc.), data = pin 11, clock = pin 13.  For Arduino Mega, data = pin 51,
 // clock = pin 52.  For 32u4 Breakout Board+ and Teensy, data = pin B2,
 // clock = pin B1.  For Leonardo, this can ONLY be done on the ICSP pins.
